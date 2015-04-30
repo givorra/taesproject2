f7dc2e663fb50f895b34f28ec3874310743d6f5d
fjg93 <fjgalianacano@gmail.com>
2015-04-30 13:34:33 +0200

    Implementacion de la interfaz, tema boton

--- Farmacia/app/src/main/java/com/project2/taes/farmacia/MainActivity.java
+++ Farmacia/app/src/main/java/com/project2/taes/farmacia/MainActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,6 +24,37 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        ImageView icon = new ImageView(this); //Con ello creamos el icono
+        icon.setImageResource(R.drawable.ic_action_floatingbutton);
+
+        FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+                .setContentView(icon)
+                .build();
+
+        //Con esto creamos cada boton del submenu
+        ImageView iconNewReport = new ImageView(this);
+        iconNewReport.setImageResource(R.drawable.ic_action_report);
+
+        ImageView iconSendReport = new ImageView(this);
+        iconSendReport.setImageResource(R.drawable.ic_action_send);
+
+        ImageView iconNewProduct = new ImageView(this);
+        iconNewProduct.setImageResource(R.drawable.ic_action_product);
+
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+
+        SubActionButton buttoniconNewReport = itemBuilder.setContentView(iconNewReport).build();
+        SubActionButton buttoniconSendReport = itemBuilder.setContentView(iconSendReport).build();
+        SubActionButton buttoniconNewProduct = itemBuilder.setContentView(iconNewProduct).build();
+
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(buttoniconNewReport)
+                .addSubActionView(buttoniconSendReport)
+                .addSubActionView(buttoniconNewProduct)
+                .attachTo(actionButton)
+                .build();
+
         return true;
     }
 
@@ -36,4 +72,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
