e2c30d70dab20ace3e2e0e845273499a340867cb
fjg93 <fjgalianacano@gmail.com>
2015-05-18 20:29:04 +0200

    Nuevo reporte_modificacion

---
+++ Farmacia/app/src/main/java/com/project2/taes/farmacia/adapters/AdapterListaProducto.java
@@ -0,0 +1,42 @@
+package com.project2.taes.farmacia.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.project2.taes.farmacia.R;
+import com.project2.taes.farmacia.classes.Producto;
+
+import java.util.ArrayList;
+
+public class AdapterListaProducto extends ArrayAdapter<Producto> {
+
+    private ArrayList<Producto> items;
+
+    public AdapterListaProducto(Context context, int textViewResourceId, ArrayList<Producto> items){
+        super(context,textViewResourceId,items);
+        this.items=items;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View v = convertView;
+        if (v==null){
+            LayoutInflater vi=(LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.productos_list, null);
+        }
+
+        Producto producto= items.get(position);
+        if(producto != null){
+            TextView ttitulo=(TextView) v.findViewById(R.id.lbNombredelProducto);
+
+            if(ttitulo != null){
+                ttitulo.setText(producto.getTitulo());
+            }
+        }
+        return v;
+    }
+}
