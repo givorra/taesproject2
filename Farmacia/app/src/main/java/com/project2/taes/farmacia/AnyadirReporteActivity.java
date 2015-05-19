c2b6007ab697ba133c72911827247ba19d5c4e35
fjg93 <fjgalianacano@gmail.com>
2015-05-20 01:52:03 +0200

    Anyadido el guardar

--- Farmacia/app/src/main/java/com/project2/taes/farmacia/AnyadirReporteActivity.java
+++ Farmacia/app/src/main/java/com/project2/taes/farmacia/AnyadirReporteActivity.java
@@ -1,17 +1,84 @@
 package com.project2.taes.farmacia;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Date;
 
 
 public class AnyadirReporteActivity extends ActionBarActivity {
+
+    TextView nomMedico;
+    EditText tnomMedico;
+
+    TextView area;
+    EditText tarea;
+
+    TextView lugar;
+    EditText tlugar;
+
+    TextView acompanyante;
+    EditText tacompanyante;
+
+    TextView fecha;
+    EditText tfecha;
+
+    TextView observaciones;
+    EditText tobservaciones;
+
+    Button botGuardar;
+    Button botCancelar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anyadir_reporte);
+
+        nomMedico=(TextView) findViewById(R.id.lbMedico);
+        tnomMedico=(EditText) findViewById(R.id.txMedico);
+
+        area= (TextView) findViewById(R.id.lbArea);
+        tarea= (EditText)findViewById(R.id.txArea);
+
+        lugar=(TextView) findViewById(R.id.lbArea);
+        tlugar=(EditText) findViewById(R.id.txArea);
+
+        acompanyante=(TextView) findViewById(R.id.lbAcompanyante);
+        tacompanyante=(EditText) findViewById(R.id.txtAcompanayante);
+
+        fecha=(TextView) findViewById(R.id.lbFecha);
+        tfecha=(EditText) findViewById(R.id.txFecha);
+
+        observaciones=(TextView) findViewById(R.id.lbObservaciones);
+        tobservaciones=(EditText) findViewById(R.id.txObservaciones);
+
+        botGuardar=(Button)findViewById(R.id.btGuardar);
+        botCancelar=(Button)findViewById(R.id.btCancelar);
+
+    }
+
+    public void guardar(View view){
+        String med=tarea.getText().toString();
+        String are=tarea.getText().toString();
+        String lug=tlugar.getText().toString();
+        Boolean acom=tacompanyante.getText().toString();
+        Date fec=tfecha.getDate().toString();
+        String obs=tobservaciones.getText().toString();
+
+
+        Reporte reporte=new Reporte(are, med, lug, acom, obs, fec);
+    }
+
+    @Override
+    public OnClick(View view){
+        reporte.guardar(getApplicationContext());
     }
 
     @Override
