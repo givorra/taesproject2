37eb3bedf7e1abe0c2c0b877c3b38187f6f6645f
ukswat <mattwebb.ynwa@gmail.com>
2015-05-20 12:52:28 +0200

    DatePickerDialog en anyadir reporte
    Hay que hacer click 2 veces para que salga el dialogo al elegir fecha.
    Anyadido scrollview

--- Farmacia/app/src/main/java/com/project2/taes/farmacia/AnyadirReporteActivity.java
+++ Farmacia/app/src/main/java/com/project2/taes/farmacia/AnyadirReporteActivity.java
@@ -1,18 +1,26 @@
 package com.project2.taes.farmacia;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 
-public class AnyadirReporteActivity extends ActionBarActivity {
+public class AnyadirReporteActivity extends ActionBarActivity implements View.OnClickListener {
 
     TextView nomMedico;
     EditText tnomMedico;
@@ -24,7 +32,7 @@ public class AnyadirReporteActivity extends ActionBarActivity {
     EditText tlugar;
 
     TextView acompanyante;
-    EditText tacompanyante;
+    CheckBox tacompanyante;
 
     TextView fecha;
     EditText tfecha;
@@ -35,50 +43,76 @@ public class AnyadirReporteActivity extends ActionBarActivity {
     Button botGuardar;
     Button botCancelar;
 
+    private DatePickerDialog datePickerDialog;
+
+    private DateFormat dateFormatter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anyadir_reporte);
 
-        TextView nomMedico=(TextView) findViewById(R.id.lbMedico);
-        EditText tnomMedico=(EditText) findViewById(R.id.txMedico);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        TextView area= (TextView) findViewById(R.id.lbArea);
-        EditText tarea= (EditText)findViewById(R.id.txArea);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
-        TextView lugar=(TextView) findViewById(R.id.lbArea);
-        EditText tlugar=(EditText) findViewById(R.id.txArea);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
-        TextView acompanyante=(TextView) findViewById(R.id.lbAcompanyante);
-        EditText tacompanyante=(EditText) findViewById(R.id.txtAcompanayante);
+        nomMedico = (TextView) findViewById(R.id.lbMedico);
+        tnomMedico = (EditText) findViewById(R.id.txMedico);
 
-        TextView fecha=(TextView) findViewById(R.id.lbFecha);
-        EditText tfecha=(EditText) findViewById(R.id.txFecha);
+        area = (TextView) findViewById(R.id.lbArea);
+        tarea = (EditText) findViewById(R.id.txArea);
 
-        TextView observaciones=(TextView) findViewById(R.id.lbObservaciones);
-        EditText tobservaciones=(EditText) findViewById(R.id.txObservaciones);
+        lugar = (TextView) findViewById(R.id.lbArea);
+        tlugar = (EditText) findViewById(R.id.txArea);
 
-        Button botGuardar=(Button)findViewById(R.id.btGuardar);
-        Button botCancelar=(Button)findViewById(R.id.btCancelar);
+        acompanyante = (TextView) findViewById(R.id.lbAcompanyante);
+        tacompanyante = (CheckBox) findViewById(R.id.checkBox);
+
+        fecha = (TextView) findViewById(R.id.lbFecha);
+        tfecha = (EditText) findViewById(R.id.txFecha);
+        tfecha.setOnClickListener(this);
+
+        observaciones = (TextView) findViewById(R.id.lbObservaciones);
+        tobservaciones = (EditText) findViewById(R.id.txObservaciones);
+
+        botGuardar = (Button) findViewById(R.id.btGuardar);
+        botCancelar = (Button) findViewById(R.id.btCancelar);
+
+        Calendar newCalendar = Calendar.getInstance();
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                tfecha.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
     }
 
-    public void guardar(View view){
-        String med=tarea.getText().toString();
-        String are=tarea.getText().toString();
-        String lug=tlugar.getText().toString();
-        Boolean acom=tacompanyante.getText().toString();
-        Date fec=tfecha.get();
-        String obs=tobservaciones.getText().toString();
+    public void guardar(View view) {
+        String med = tnomMedico.getText().toString();
+        String are = tarea.getText().toString();
+        String lug = tlugar.getText().toString();
+        Boolean acom = tacompanyante.isChecked();
+        //Date fec=tfecha;
+        String obs = tobservaciones.getText().toString();
 
 
-        Reporte reporte=new Reporte(are, med, lug, acom, obs, fec);
-    }
-
-    @Override
-    public OnClick(View view){
-        reporte.guardar(getApplicationContext());
+        //Reporte reporte=new Reporte(are, med, lug, acom, obs, fec);
+        //reporte.guardar(getApplicationContext());
     }
 
     @Override
@@ -101,5 +135,12 @@ public class AnyadirReporteActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == tfecha) {
+            datePickerDialog.show();
+        }
     }
 }
